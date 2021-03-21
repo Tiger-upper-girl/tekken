b005707ccd503f43e8dfe1a3ef34a1e330644800
Tiger-upper-girl <81090649+Tiger-upper-girl@users.noreply.github.com>
2021-03-21 23:07:35 +0900

    Add files via upload

--- tekken/index.js
+++ tekken/index.js
@@ -1,3 +1,5 @@
+// (()=>{
+
 'use strict';
 //ゲームスタート、プレイヤー名入力イベント
 function butotnClick1(){
@@ -183,6 +185,7 @@ const tk = [
   }
   }
 
+// })();
 
 
 
@@ -191,51 +194,3 @@ const tk = [
 
 
 
-
-
-
-// const quizLength = quiz.length;
-// let quizIndex = 0;
-// let score = 0;
-
-// const $button = document.getElementsByTagName('button');
-// const buttonLength = $button.length;
-
-// //クイズの問題文、選択肢を定義
-// const setupQuiz = () => {
-//   document.getElementById('js-question').textContent = quiz[quizIndex].question;
-//   let buttonIndex = 0;
-//   while(buttonIndex < buttonLength){
-//     $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
-//     buttonIndex++;
-//   }
-// }
-// setupQuiz();
-
-// //ボタンクリック正誤判定
-
-// const clickHandler = (e) => {
-//   if(quiz[quizIndex].correct === e.target.textContent){
-//     window.alert('正解！');
-//   } else{
-//     window.alert('不正解！');
-//     score++
-//   }
-
-//   quizIndex++;
-
-//   if(quizIndex < quizLength){
-//     setupQuiz();
-//   }else{
-//     window.alert('終了！あなたの正解数は' + (quizLength-score) + '/' + quizLength + 'です！');
-//   }
-
-// };
-
-// let handleIndex = 0;
-// while (handleIndex < buttonLength) {
-//   $button[handleIndex].addEventListener('click', (e) => {
-//     clickHandler(e);
-//   });
-//   handleIndex++;
-// }
